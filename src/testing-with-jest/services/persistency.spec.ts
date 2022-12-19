ac573dcd5ba0241a936d6cb522e95377d91afda4
Thiago Souza <thiagofernandosouza20@gmail.com>
2022-12-19 11:09:48 -0300

    jest assertions

---
+++ src/testing-with-jest/services/persistency.spec.ts
@@ -0,0 +1,13 @@
+describe("TESTANDO ALGUMA COISA (it)", () => {
+     it("descricao do teste (it)", () => {
+          const number = 1
+          expect(number).toBe(1)
+     })
+})
+
+describe("TESTANDO ALGUMA COISA (test)", () => {
+     test("descricao do teste (test)", () => {
+          const nome = "thiago"
+          expect(nome).toBe("thiago")
+     })
+})
